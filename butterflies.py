67e94db54d2a98c4de2dffde040cf0a19140d42f
Johan Manuel <johan.manuel@live.fr>
2014-08-27 21:47:08 +0200

    1st working version, but random mutation is missing

--- butterflies.py
+++ butterflies.py
@@ -13,35 +13,41 @@ class Butterfly(pygame.sprite.Sprite):
             self.randomize()
         else:
             self.setColor(color)
-            if position != None:
-                self.rect.x = position.x
-                self.rect.y = position.y
+
+        if position != None:
+            self.rect.x = position.x
+            self.rect.y = position.y
+        else:
+            self.rect.x = randrange(width-self.rect.w)
+            self.rect.y = randrange(height-self.rect.h)
 
     def randomize(self):
         self.rect.x = randrange(width-self.rect.w)
         self.rect.y = randrange(height-self.rect.h)
-        rdcolor = (randrange(255),randrange(255),randrange(255))
-        self.setColor(rdcolor)
+        color = (randrange(255),randrange(255),randrange(255))
+        self.setColor(color)
 
     def setColor(self, color):
-        self.color = [col for col in ''.join(format(c, '08b') for c in color)]
+        self.color = color
+        self.gene = [col for col in ''.join(format(c, '08b') for c in color)]
         pxarray = pygame.PixelArray(self.image)
         pxarray.replace((255, 255, 255), color)
 
 def crossover(mum, dad):
     if random.uniform(0, 1) < crossover_rate:
-        pos = randrange(len(mum.color))
-        for i in range(pos, len(dad.color)):
-            mum.color[i], dad.color[i] = dad.color[i], mum.color[i]
+        pos = randrange(len(mum.gene))
+        for i in range(pos, len(dad.gene)):
+            mum.gene[i], dad.gene[i] = dad.gene[i], mum.gene[i]
 
 def getNewPopulation(old_pop):
     pop = []
-    for couple in itertools.combinations(old_pop, 2):
-        if len(pop) >= pop_size:
-            break
-        crossover(couple[0], couple[1])
-        pop.append(couple[0])
-        pop.append(couple[1])
+    while len(pop) < pop_size:
+        for couple in itertools.combinations(old_pop, 2):
+            if len(pop) >= pop_size:
+                break
+            crossover(couple[0], couple[1])
+            pop.append(Butterfly(couple[0].color))
+            pop.append(Butterfly(couple[1].color))
     return pop
 
 size = width, height = 800, 600
@@ -49,38 +55,35 @@ size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 
 crossover_rate = 0.7
-pop_size = 30
-allowed_time = 1000*15 # 20 sec in millliseconds
-timer_event = pygame.USEREVENT+1
+pop_size = 60
+allowed_time = 1000*10 # 20 sec in millliseconds
+respawn_event = pygame.USEREVENT+1
 
 def main():
-    population = [Butterfly() for i in range(pop_size)]
-    pygame.time.set_timer(timer_event, allowed_time)
+    pop = [Butterfly() for i in range(pop_size)]
+    pygame.time.set_timer(respawn_event, allowed_time)
+
+    running = True
+    respawn = False
     while True:
-        if len(population) <= 1:
-            print("You've extermined this butterfly specy !")
-        timed_out = False
-        print("outside timed loop")
-        while not timed_out:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    print("exiting...")
-                    sys.exit(0)
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    for b in population:
-                        if b.rect.collidepoint(event.pos):
-                            population.remove(b)
-                            break
-                if event.type == timer_event:
-                    print(len(population))
-                    population = getNewPopulation(population)
-                    print(len(population))
-                    timed_out = True
-            screen.fill((255, 255, 255))
-            for b in population:
-                screen.blit(b.image, b.rect)
-            pygame.display.flip()
-            pygame.time.delay(10)
+        if respawn:
+            pop = getNewPopulation(pop)
+        respawn = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for butterfly in pop:
+                    if butterfly.rect.collidepoint(event.pos):
+                        pop.remove(butterfly)
+                        break
+            if event.type == respawn_event:
+                respawn = True
+
+        screen.fill((255, 255, 255))
+
+        for butterfly in pop:
+            screen.blit(butterfly.image, butterfly.rect)
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
